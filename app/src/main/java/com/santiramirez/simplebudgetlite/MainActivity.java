784027b0011aabb279cbbff7cd87693122bd2384
Santiago Ramirez <santi.wildcat.12@gmail.com>
2014-11-17 18:31:30 -0500

    Fixed indents

--- app/src/main/java/com/santiramirez/simplebudgetlite/MainActivity.java
+++ app/src/main/java/com/santiramirez/simplebudgetlite/MainActivity.java
@@ -19,59 +19,60 @@ import android.webkit.WebSettings;
 import android.webkit.WebViewClient;
 import android.content.Context;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity
+{
     private static final String TAG = "MainActivity";
     private final Handler handler = new Handler();
     private WebView WebView;
     private TextView TextView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onCreate( Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
 
-        WebView = (WebView) findViewById(R.id.web_view);
+        WebView = ( WebView ) findViewById( R.id.web_view );
         WebSettings settings = WebView.getSettings();
 
-        settings.setJavaScriptEnabled(true);
-        settings.setDatabaseEnabled(true);
-        String databasePath = this.getApplicationContext().getDir("database", Context.MODE_PRIVATE).getPath();
+        settings.setJavaScriptEnabled( true );
+        settings.setDatabaseEnabled( true );
+        String databasePath = this.getApplicationContext().getDir( "database", Context.MODE_PRIVATE ).getPath();
         settings.setDatabasePath(databasePath);
 
-        class AndroidBridge {
-
-            public void callAndroid( final String arg){
-                handler.post( new Runnable() {
+        class AndroidBridge
+        {
+            public void callAndroid( final String arg )
+            {
+                handler.post( new Runnable()
+                {
                     @Override
-                    public void run() {
-                        Log.d(TAG, "callAndroid(" + arg + ")");
-                        TextView.setText(arg);
+                    public void run(){
+                        Log.d( TAG, "callAndroid(" + arg + ")" );
+                        TextView.setText( arg );
                     }
                 });
             }
         }
 
-        WebView.setWebContentsDebuggingEnabled(false);
-        WebView.setVerticalScrollBarEnabled(false);
-        WebView.addJavascriptInterface( new AndroidBridge(), "android");
-        WebView.getSettings().setJavaScriptEnabled(true);
+        WebView.setWebContentsDebuggingEnabled( false );
+        WebView.setVerticalScrollBarEnabled( false );
+        WebView.addJavascriptInterface( new AndroidBridge(), "android" );
+        WebView.getSettings().setJavaScriptEnabled( true );
 
-        WebView.setWebChromeClient(new WebChromeClient() {
+        WebView.setWebChromeClient( new WebChromeClient()
+        {
+            public boolean onJsAlert( final WebView view, final String url, final String message, JsResult result )
+            {
+                Log.d( TAG, "onJsAlert(" + view + ", " + url + ", " + message + ", " + result + ")" );
 
-
-            public boolean onJsAlert(final WebView view, final String url, final String message,
-                                     JsResult result) {
-                Log.d(TAG, "onJsAlert(" + view + ", " + url + ", " + message + ", " + result + ")");
-
-                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                Toast.makeText( MainActivity.this, message, Toast.LENGTH_SHORT ).show();
                 result.confirm();
                 return true;
-
             }
         });
 
-        settings.setDomStorageEnabled(true);
-        WebView.loadUrl("file:///android_asset/overview.html");
+        settings.setDomStorageEnabled( true );
+        WebView.loadUrl( "file:///android_asset/overview.html" );
     }
 }
